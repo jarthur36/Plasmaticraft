f2f0a3cf755671892a42a0e02add08e73bcc83b8
Matheus m Alcantara <matheus.de.alcantara@gmail.com>
2013-03-20 19:47:37 -0300

    Updated BlockEnetManager. FIXME : Wrench dont destoy it

--- common/pc/blocks/BlockEnetManager.java
+++ common/pc/blocks/BlockEnetManager.java
@@ -1,6 +1,7 @@
 package Plasma.common.pc.blocks;
 
 import Plasma.common.pc.Plasmaticraft;
+import Plasma.common.pc.core.CommonProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,14 +12,31 @@ public class BlockEnetManager extends Block{
 
 	public BlockEnetManager() {
 		super(1001, 1, Material.iron);
+		setCreativeTab(CommonProxy.CTAB);
+		setTextureFile(CommonProxy.BLOCK);
 	}
 	public boolean onBlockActivated(World world, int x, int y, int z,
             EntityPlayer player, int idk, float what, float these, float are) {
-			if(!player.isSneaking() && player.getItemInUse() == new ItemStack(Plasmaticraft.pWrench)) {
-				world.setBlock(x, y, z, blockID);
+			if(player.isSneaking() && player.getItemInUse() == new ItemStack(Plasmaticraft.getpWrench())) {
+				world.setBlock(x, y, z, 0);
 				player.inventory.addItemStackToInventory(new ItemStack(this,1));
 			}
 				return true;
   
 	} 
+	 public int getBlockTextureFromSide(int i) 
+     
+     {
+             if(i == 1) 
+             {
+                     return 2;
+             }
+             if(i == 0)
+             {
+                     return 2;
+             }else
+             {
+                     return blockIndexInTexture;
+             }
+     }
 }
